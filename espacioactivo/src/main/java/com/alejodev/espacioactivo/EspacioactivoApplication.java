2fec55d74e7c96b9a61041f69189e70ca3c7bd05
alergio <alejosmaya8@gmail.com>
2024-04-21 16:55:00 -0300

    inicializo el proyecto de spring

---
+++ espacioactivo/src/main/java/com/alejodev/espacioactivo/EspacioactivoApplication.java
@@ -0,0 +1,13 @@
+package com.alejodev.espacioactivo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EspacioactivoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EspacioactivoApplication.class, args);
+	}
+
+}
---
+++ espacioactivo/src/test/java/com/alejodev/espacioactivo/EspacioactivoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alejodev.espacioactivo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EspacioactivoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
